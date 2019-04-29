d7b3c7cd8d58b7273d6e5c40885aaf6cac743ade
macterra <davidmc@gmail.com>
2019-04-29 21:28:06 +0000

    Tagged v.0.4.29

--- rebuild.sh
+++ rebuild.sh
@@ -2,4 +2,5 @@
 TAG=`git rev-parse --short HEAD`
 docker image build -t macterra/tesseract-node:$TAG .
 docker image tag macterra/tesseract-node:$TAG macterra/tesseract-node:latest
+docker image tag macterra/tesseract-node:$TAG macterra/tesseract-node:v0.04.29
 
