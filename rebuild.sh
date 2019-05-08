2e0a40b8099fda9401f49cc1778906233658b8e2
macterra <davidmc@gmail.com>
2019-05-08 22:41:34 +0000

    Statoshi branch

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 TAG=`git rev-parse --short HEAD`
 docker image build -t macterra/tesseract-node:$TAG .
-docker image tag macterra/tesseract-node:$TAG macterra/tesseract-node:latest
-docker image tag macterra/tesseract-node:$TAG macterra/tesseract-node:v0.05.06
+docker image tag macterra/tesseract-node:$TAG macterra/tesseract-node:statoshi
 
