5c74305d065507b1f6af080974ad7859ff73a03c
macterra <davidmc@gmail.com>
2019-06-18 22:53:03 +0000

    Rebased on master

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 TAG=`git rev-parse --short HEAD`
 docker image build -t macterra/tesseract-node:$TAG .
-docker image tag macterra/tesseract-node:$TAG macterra/tesseract-node:statoshi
+docker image tag macterra/tesseract-node:$TAG macterra/tesseract-node:statoshi2
 
